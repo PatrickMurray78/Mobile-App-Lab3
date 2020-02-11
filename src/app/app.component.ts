e8086d95e9dfa79b5aa5d943a7e2ee4cca317de6
patrickmurray73 <g00344530@gmit.ie>
2020-02-11 17:25:40 +0000

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'root-directive',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'my-first-app';
+
+  name:string = 'Paddy';
+  age:number = 21;
+}
